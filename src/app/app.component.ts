8c516c1759f20e1c43822a75e8c8c4bc65548ab0
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-14 16:54:42 -0300

    implementing modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  ModalRef,
+  ModalService,
+} from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'cirilo_accessibility_modal';
+  public firstName = 'Cirilo';
+  public modalRef: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  public show(): void {
+    this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
 }
