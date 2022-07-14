2634e80d41d1575a653cbaf3cf8006a6ff3f8a62
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-14 18:36:15 -0300

    implementing modal service

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,20 +1,42 @@
-import { Injectable, TemplateRef } from '@angular/core';
+import {
+  ComponentFactory,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Injectable,
+  Injector,
+} from '@angular/core';
+import { ModalConfig } from '../interfaces/modal-config';
+import { ModalComponent } from '../modal.component';
 
 @Injectable()
 export class ModalService {
+  private componentFactory: ComponentFactory<ModalComponent>;
+
+  constructor(
+    componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector
+  ) {
+    this.componentFactory =
+      componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
+
   public open(config: ModalConfig): ModalRef {
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config = config;
     console.log('open called');
-    return new ModalRef();
+    return new ModalRef(componentRef);
+  }
+
+  private createComponentRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
   }
 }
 
-export interface ModalConfig {
-  templateRef: TemplateRef<any>;
-  title: string;
-}
-
 export class ModalRef {
+  constructor(private componentRef: ComponentRef<ModalComponent>) {}
+
   public close(): void {
     console.log('close called');
+    this.componentRef.destroy();
   }
 }
