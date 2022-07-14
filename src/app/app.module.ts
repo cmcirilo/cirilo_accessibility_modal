d4735d7099dfe74c8153f66257a492f55ef875c8
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-07-14 15:45:12 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 
 @NgModule({
   declarations: [
@@ -11,9 +10,7 @@ import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/
   ],
   imports: [
     BrowserModule,
-    YesNoButtonGroupModule,
-    ReactiveFormsModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
